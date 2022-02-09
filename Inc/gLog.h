6e32ed808bd2adfab6e46bbccc0377864a636c8f
Talld <ewain.williams@gmail.com>
2022-02-09 22:56:40 +0000

    Fixed cmake

--- Inc/gLog.h
+++ Inc/gLog.h
@@ -3,39 +3,42 @@
 
 #include <cstdio>
 
-// compiler specific built in's
-#ifdef __GNUC__
-	#define GLOG_DEBUG_BREAK __builtin_trap
-#endif // __GNUC__
 
-#ifdef __clang__
+// compiler specific built in's
+
+#if __clang__
 	#define GLOG_DEBUG_BREAK __builtin_debugtrap
-#endif // __clang__
+#elif __GNUC__
+	#define GLOG_DEBUG_BREAK __builtin_trap
+#endif
 
 // logging
 #ifdef GLOG_LOGGING_ENABLED
 #define GLOG_ERR(...)  fprintf(stderr, " %s::%lu ",__FILE__, __LINE__);  fprintf(stderr, __VA_ARGS__)
 #define GLOG_INFO(...) fprintf(stderr, " %s::%lu ",__FILE__, __LINE__);  fprintf(stdout, __VA_ARGS__)
+#else
+#define GLOG_ERR(...)
+#define GLOG_INFO(...)
 #endif
 
 // an assert will just log an error but not kill
 #define GLOG_ASSERT(con, msg)         \
 do                                    \
-	if ( !(con) )                 \
-	{                             \
-		GLOG_ERR(msg)         \
-	}                             \
+	if ( !(con) )                     \
+	{                                 \
+		GLOG_ERR(msg)                 \
+	}                                 \
 while(0)
 // the do while is so a semi-colon can be added at the end when in use
 
 // whereas require will kill the app if the conditions aren't met
-#define GLOG_REQUIRE(con, msg) \
+#define GLOG_REQUIRE(con, msg)        \
 do                                    \
-	if ( !(con) )                 \
-	{                             \
-		GLOG_ERR(msg);        \
-                GLOG_DEBUG_BREAK();   \
-	}                             \
+	if ( !(con) )                     \
+	{                                 \
+		GLOG_ERR(msg);                \
+		GLOG_DEBUG_BREAK();           \
+	}                                 \
 while(0)
 
 
