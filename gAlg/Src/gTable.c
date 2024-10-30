a35356beda72650b66a6c12596666bff13bf5da5
Ewain williams <ewain.dude@gmail.com>
2024-10-30 02:10:48 +0000

    add unsigned types

--- gAlg/Src/gTable.c
+++ gAlg/Src/gTable.c
@@ -2,11 +2,14 @@
 #include <gTable.h>
 #include <gTable.inc>
 
-GTABLE_TYPE_SRC(void* , Ptr);
-
-GTABLE_TYPE_SRC(float , Float);
-GTABLE_TYPE_SRC(double , Double);
-
-GTABLE_TYPE_SRC(int64_t, Int64);
-GTABLE_TYPE_SRC(int32_t, Int32);
-GTABLE_TYPE_SRC(int16_t, Int16);
+GTABLE_TYPE_SRC( void*,    Ptr    );
+GTABLE_TYPE_SRC( float,    Float  );
+GTABLE_TYPE_SRC( double,   Double );
+GTABLE_TYPE_SRC( int64_t,  Int64  );
+GTABLE_TYPE_SRC( int32_t,  Int32  );
+GTABLE_TYPE_SRC( int16_t,  Int16  );
+GTABLE_TYPE_SRC( int8_t,   Int8   );
+GTABLE_TYPE_SRC( uint64_t, UInt64 );
+GTABLE_TYPE_SRC( uint32_t, UInt32 );
+GTABLE_TYPE_SRC( uint16_t, UInt16 );
+GTABLE_TYPE_SRC( uint8_t,  UInt8  );
